4f74fc2325378c7b3ef5dee52e68a9180b56893c
vadim <bigulipa@gmail.com>
2023-11-12 15:26:40 +0400

    added the ability to filter posts by several tags at once

--- blog/views.py
+++ blog/views.py
@@ -16,11 +16,28 @@ def post_detail(request, pk):
 
 def tag_filter(request):
     tag_name = request.GET.get("tag")
-    posts = Post.objects.filter(tags__name=tag_name) if tag_name else Post.objects.all()
+    action = request.GET.get("action")
+    selected_tags = request.session.get("selected_tags", [])
+
+    if tag_name:
+        if action == "add" and tag_name not in selected_tags:
+            selected_tags.append(tag_name)
+        elif action == "remove" and tag_name in selected_tags:
+            selected_tags.remove(tag_name)
+        else:
+            selected_tags = [tag_name]
+
+    posts = Post.objects.all()
+
+    if selected_tags:
+        posts = posts.filter(tags__name__in=selected_tags)
+
     tags = Tag.get_all_tags()
+
+    request.session["selected_tags"] = selected_tags
 
     return render(
         request,
         "blog/post_list.html",
-        {"posts": posts, "tags": tags, "selected_tag": tag_name},
+        {"posts": posts, "tags": tags, "selected_tags": selected_tags},
     )
