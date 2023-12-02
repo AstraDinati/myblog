26a629cfe2fd1acdf53bb7bb0900a28876e248fa
vadim <bigulipa@gmail.com>
2023-12-02 04:57:22 +0400

    the front of the page of a separate post has been improved, the front of the About page has been created, minor fixes to the front

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -10,9 +10,7 @@ function toggleTagsCloud() {
 
 function toggleMobileMenu() {
     var navMenu = document.querySelector('.nav-menu');
-    var socLin = document.querySelector('.social-links');
     navMenu.classList.toggle('show-mobile');
-    socLin.classList.toggle('show-mobile');
 }
 
 // Добавляем слушатель событий для закрытия меню при клике вне его пределов
@@ -30,37 +28,19 @@ document.addEventListener('click', function (event) {
     }
 });
 
-
-// Добавляем слушатель событий для закрытия меню при клике вне его пределов
 document.addEventListener('click', function (event) {
     var tagCont = document.querySelector('.tag-container');
     var targetElement = event.target;
 
-    // Проверяем, является ли кликнутый элемент частью бургера или меню
-    var isClickInsideTagList = targetElement.closest('.tag-list');
-    var isClickInsideContainer = targetElement.closest('.tag-container');
-
-    if (!isClickInsideTagList && !isClickInsideContainer) {
-        // Закрываем меню, если клик был вне его пределов
-        tagCont.classList.remove('show');
-    }
-});
-
-
-// Добавьте следующий код для закрытия панели тегов при клике за её пределами
-document.addEventListener('click', function (event) {
-    var tagCont = document.querySelector('.tag-container');
-    var targetElement = event.target; // Кликнутый элемент
-
-    // Проверяем, является ли кликнутый элемент кнопкой для открытия панели тегов
-    if (targetElement.classList.contains('nav-link') && targetElement.textContent.trim() === 'Tags') {
+    if (!tagCont) {
         return;
     }
 
-    // Проверяем, является ли кликнутый элемент частью панели тегов
-    if (!tagCont.contains(targetElement)) {
-        // Если не является, закрываем панель тегов
-        tagCont.classList.remove('show-mobile');
+    var isClickInsideTagList = targetElement.closest('.tag-list');
+    var isClickInsideContainer = targetElement.closest('.tag-container');
+
+    // Проверяем, существует ли класс 'show' у элемента
+    if (tagCont.classList && tagCont.classList.contains('show') && !isClickInsideTagList && !isClickInsideContainer) {
+        tagCont.classList.remove('show');
     }
 });
-
